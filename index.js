526ea28ab43102149b7dbf92aa67634999bda7a5
XIC905Bipund <bipund.pandey@xebia.com>
2023-02-28 01:12:30 +0530

    kjnfs

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
 const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const db = require('./mysql_connection');
-const path = require('path');
-const cors = require('cors');
+const mysql = require("mysql");
+// const db = require('./mysql_connection');
+const path = require("path");
+const cors = require("cors");
 app.use(cors());
-const fs=require('fs');
-const dotenv = require('dotenv');
-dotenv.config({path: './.env'});
+const fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 
-
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
 const allowedExt = [
-    '.js',
-    '.ico',
-    '.css',
-    '.png',
-    '.jpg',
-    '.gif',
-    '.jpeg',
-    '.woff2',
-    '.woff',
-    '.ttf',
-    '.svg',
-    '.mp4',
-  ];
-  app.engine('html', require('ejs').renderFile);
-  app.use(express.static(__dirname + './build'));
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+  ".js",
+  ".ico",
+  ".css",
+  ".png",
+  ".jpg",
+  ".gif",
+  ".jpeg",
+  ".woff2",
+  ".woff",
+  ".ttf",
+  ".svg",
+  ".mp4",
+];
+app.engine("html", require("ejs").renderFile);
+app.use(express.static(__dirname + "./build"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-
-app.use('/user',require('./routes/user_routes'));
-app.use('/admin',require('./routes/admin_routes'));
+app.use("/user", require("./routes/user_routes"));
+app.use("/admin", require("./routes/admin_routes"));
 // app.use('/support',require('./routes/support_routes'));
-app.use('/search',require('./routes/search_routes'));
-app.use('/booking',require('./routes/booking_routes'));
-app.use('/gateway',require('./routes/payment_routes'));
-app.use('/site',require('./routes/site_route'));
+app.use("/search", require("./routes/search_routes"));
+app.use("/booking", require("./routes/booking_routes"));
+app.use("/gateway", require("./routes/payment_routes"));
+app.use("/site", require("./routes/site_route"));
 
 // app.use('/payment',require('./routes/payment_routes'));
 // app.use('/location',require('./routes/car_location_routes'));
 // app.use('/blog',require('./routes/blog_routes'));
 // app.use('/FAQ',require('./routes/faq_routes'));
 
-// app.get('/', function(req, res){ 
-//     res.render('pay', { 
-//     key: process.env.stripe_Publishable_Key 
-//     }) 
+// app.get('/', function(req, res){
+//     res.render('pay', {
+//     key: process.env.stripe_Publishable_Key
+//     })
 // })
 
 // app.get('/', (req, res, next) => {
@@ -65,27 +62,28 @@ app.use('/site',require('./routes/site_route'));
 // })
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
-  
-  
-  
-  // routes by Bipund
-  const html = './build/'
-  
-  app.get('*', function (req, res, next) {
-    if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
-      res.sendFile(path.resolve(`./build/${req.url}`));
-    } else {
-      res.sendFile('index.html', {
-        root: html
-      });
-    }
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+// routes by Bipund
+const html = "./build/";
+
+app.get("*", function (req, res, next) {
+  if (allowedExt.filter((ext) => req.url.indexOf(ext) > 0).length > 0) {
+    res.sendFile(path.resolve(`./build/${req.url}`));
+  } else {
+    res.sendFile("index.html", {
+      root: html,
+    });
+  }
+});
 // app.post('/log/success', (req, res, next) => {
 //     //res.sendFile('./abc.html');
 //     console.log(req);
@@ -93,5 +91,5 @@ app.use(function (req, res, next) {
 // })
 
 app.listen(3001, (req, res) => {
-    console.log('server running....');
-})
+  console.log("server running....");
+});
--- mysql_connection.js
+++ mysql_connection.js
@@ -1,25 +1,25 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv');
-dotenv.config({path: './.env'});
+// const mysql = require('mysql');
+// const dotenv = require('dotenv');
+// dotenv.config({path: './.env'});
 
-const db=mysql.createPool({
-    connectionlimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE
-});
+// const db=mysql.createPool({
+//     connectionlimit: 10,
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DATABASE
+// });
 
-db.getConnection((error, conn)=>{
-    if(error){
-        console.log("eror",error);
-    }
-    else{
-        console.log("mysql connected");
-        conn.rollback();
-        conn.release();
-        
-    }
-});
+// db.getConnection((error, conn)=>{
+//     if(error){
+//         console.log("eror",error);
+//     }
+//     else{
+//         console.log("mysql connected");
+//         conn.rollback();
+//         conn.release();
 
-module.exports = db;
+//     }
+// });
+
+// module.exports = db;
